1e6ce9a80f7110428e3ea615b95925521fac9d5c
thejsj <jorge.silva@thejsj.com>
2017-07-19 17:55:46 -0400

    Update README

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Auto Kuberentes Lets's Encrypt
 
-_Generate TSL/SSL certs for your kubernetes ingress automatically with Let's Encrypt_
+_Generate TSL/SSL certs through your kubernetes cluter automatically with Let's Encrypt_
 
 ## How To Use
 
@@ -29,5 +29,28 @@ Take the IP address from the service and update your DNS settings.
 ```
 kubectl apply -f ./kubernetes-resources-part-1.yml
 watch "kubectl get job auto-kubernetes-lets-encrypt"
-kubectl get secret auto-kubernetes-lets-encrypt
+kubectl describe secret auto-kubernetes-lets-encrypt
+```
+
+**You're Done!**
+
+You will now find the certificates for the domain in your `auto-kubernetes-lets-encrypt` secret:
+
+```
+$ kubectl describe secret auto-kubernetes-lets-encrypt
+Name:		auto-kubernetes-lets-encrypt
+Namespace:	default
+Labels:		<none>
+Annotations:
+Type:		Opaque
+
+Data
+====
+$DOMAIN.issuer.crt:	1647 bytes
+$DOMAIN.json:		228 bytes
+$DOMAIN.key:		1679 bytes
+$DOMAIN.pem:		3480 bytes
+private_key:			1675 bytes
+registration:			668 bytes
+$DOMAIN.crt:		1801 bytes
 ```
--- generate-kubernetes-resources.bash
+++ generate-kubernetes-resources.bash
@@ -16,17 +16,20 @@ if [[ -z $EMAIL ]]; then
   exit 1
 fi
 # 1. Generate Private Key
-openssl genrsa -out private-key.pem 2048 >/dev/null 2>&1
+if [ ! -f ./private-key.pem ]; then
+  openssl genrsa -out private-key.pem 2048 >/dev/null 2>&1
+fi
 # Requires newlines to be encoded
 PRIVATE_KEY_BASE64=$(cat ./private-key.pem | base64 -w 0)
 # 2. Generate Random Port
 RANDOM_INT=$(( $RANDOM % 2767 ))
 NODE_PORT=$((30000 + RANDOM_INT))
 # 3. Copy file
-cp ./kubernetes-resources.yml-part-1.tmpl ./kubernetes-resources-part-1.yml
-cp ./kubernetes-resources.yml-part-2.tmpl ./kubernetes-resources-part-2.yml
+cp ./kubernetes-resources-part-1.yml.tmpl ./kubernetes-resources-part-1.yml
+cp ./kubernetes-resources-part-2.yml.tmpl ./kubernetes-resources-part-2.yml
 # 4. Execute replacements
-sed -i -e "s/\*NODE_PORT\*/$NODE_PORT/g" kubernetes-resources-part-1.yml
-sed -i -e "s/\*PRIVATE_KEY_BASE64\*/$PRIVATE_KEY_BASE64/g" kubernetes-resources-part-1.yml
-sed -i -e "s/\*DOMAIN\*/$DOMAIN/g" kubernetes-resources-part-2.yml
-sed -i -e "s/\*EMAIL\*/$EMAIL/g" kubernetes-resources-part-2.yml
+sed -i .bak "s/\*NODE_PORT\*/$NODE_PORT/g" kubernetes-resources-part-1.yml
+sed -i .bak "s/\*PRIVATE_KEY_BASE64\*/$PRIVATE_KEY_BASE64/g" kubernetes-resources-part-1.yml
+sed -i .bak "s/\*DOMAIN\*/$DOMAIN/g" kubernetes-resources-part-2.yml
+sed -i .bak "s/\*EMAIL\*/$EMAIL/g" kubernetes-resources-part-2.yml
+rm *.yml.bak
